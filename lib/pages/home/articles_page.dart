8206a669bcf52d6ea326994da3e47280a0bfcafa
Rashid Wassan <rashidwassaan@gmail.com>
2021-09-27 15:21:33 +0500

    ui fixes

---
+++ lib/pages/home/articles_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:rashidwassan/pages/home/home.dart';
+
+class ArticlesPage extends StatelessWidget {
+  const ArticlesPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Home(
+      child: Container(),
+    );
+  }
+}
