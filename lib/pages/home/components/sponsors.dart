54790a7dab0a1b72c623505caeb2d7934bda57de
Rashid Wassan <rashidwassaan@gmail.com>
2021-12-20 00:36:07 +0500

    sponsor widget ui fix

--- lib/pages/home/components/sponsors.dart
+++ lib/pages/home/components/sponsors.dart
@@ -17,43 +17,48 @@ class Sponsors extends StatelessWidget {
     return Container(
       padding: EdgeInsets.symmetric(vertical: 50.0),
       child: ScreenHelper(
-        desktop: _buildUi(kDesktopMaxWidth),
-        tablet: _buildUi(kTabletMaxWidth),
-        mobile: _buildUi(getMobileMaxWidth(context)),
+        desktop: SponsorUI(width: kDesktopMaxWidth),
+        tablet: SponsorUI(width: kTabletMaxWidth),
+        mobile: SponsorUI(width: getMobileMaxWidth(context)),
       ),
     );
   }
 }
 
-Widget _buildUi(double width) {
-  return Center(
-    child: ResponsiveWrapper(
-      minWidth: width,
-      maxWidth: width,
-      defaultScale: false,
-      child: LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-          return Wrap(
-            alignment: WrapAlignment.center,
-            runSpacing: 50.0,
-            spacing: 50.0,
-            children: sponsorsLogo
-                .map(
-                  (logo) => Container(
-                    height: 20.0,
-                    child: Image.asset(logo),
-                    constraints: BoxConstraints(
-                      // max of 3 per row when on mobile and 5 per row on bigger screens
-                      maxWidth: ScreenHelper.isMobile(context)
-                          ? constraints.maxWidth / 3.0 - 50.0
-                          : constraints.maxWidth / 5.0 - 50.0,
+class SponsorUI extends StatelessWidget {
+  const SponsorUI({Key key, @required this.width}) : super(key: key);
+  final double width;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ResponsiveWrapper(
+        minWidth: width,
+        maxWidth: width,
+        defaultScale: false,
+        child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            return Wrap(
+              alignment: WrapAlignment.center,
+              runSpacing: 50.0,
+              spacing: 50.0,
+              children: sponsorsLogo
+                  .map(
+                    (logo) => Container(
+                      height: 20.0,
+                      child: Image.asset(logo),
+                      constraints: BoxConstraints(
+                        // max of 3 per row when on mobile and 5 per row on bigger screens
+                        maxWidth: ScreenHelper.isMobile(context)
+                            ? constraints.maxWidth / 3.0 - 50.0
+                            : constraints.maxWidth / 5.0 - 50.0,
+                      ),
                     ),
-                  ),
-                )
-                .toList(),
-          );
-        },
+                  )
+                  .toList(),
+            );
+          },
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
