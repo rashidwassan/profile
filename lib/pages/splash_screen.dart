dfa72aac01e40453c2aa6349727f39c2846e6ed8
Rashid Wassan <rashidwassaan@gmail.com>
2022-02-08 22:23:29 +0500

    splash anim responsiveness fix

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -28,23 +28,28 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            LottieBuilder.asset(
-              Images.splashAnimation,
-              frameRate: FrameRate(60),
-            ),
-            const SizedBox(height: 16),
-            Neon(
-              text: 'Preparing...',
-              color: Colors.purple,
-              font: NeonFont.LasEnter,
-              glowing: true,
-              fontSize: 25,
-              flickeringLetters: [1, 5, 8, 9, 10],
-            )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Flexible(
+                child: LottieBuilder.asset(
+                  Images.splashAnimation,
+                  frameRate: FrameRate(60),
+                ),
+              ),
+              const SizedBox(height: 16),
+              Neon(
+                text: 'Preparing...',
+                color: Colors.purple,
+                font: NeonFont.LasEnter,
+                glowing: true,
+                fontSize: 25,
+                flickeringLetters: [1, 5, 8, 9, 10],
+              )
+            ],
+          ),
         ),
       ),
     );
